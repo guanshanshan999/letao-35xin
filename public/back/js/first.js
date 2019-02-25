32ab8fe194929a64b740f3590d8ee093571ff4cb
guanshanshan999 <1257749818@qq.com>
2019-02-25 09:21:20 +0800

    user页面功能的bug解决

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -15,13 +15,30 @@ $(function(){
             },
             dataType: 'json',
             success: function (info) {
-              console.log(info);
+              // console.log(info);
         var htmlStr = template('firstTpl',info);
           $('tbody').html(htmlStr);
 
 
         // 完成分页初始化
+$('#paginator').bootstrapPaginator({
+   //版本号
+   bootstrapMajorVersion:3,
+   //当前页
+   currentPage: info.page,
+   //总页数
+   totalPages:Math.ceil(info.total/info.size),
+   //给页码添加点击事件
+   onpageClicked:function(a,b,c,page){
+        // console.log(page);
+        //更新当前页,并且重新渲染
+   currentPage = page;
+   render();
+        
+   }
 
+
+})
 
 
             }
